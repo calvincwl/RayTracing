5b3740607f9ff1e7261d33b16ec8f689d67768e3
calvincwl <calvinwl@gmail.com>
2018-05-16 00:26:31 +0800

    Add ray class

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
+#include "ray.h"
 using namespace std;
+
+vec3 color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5 * (unit_direction.y() + 1.0f);
+	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
-	fstream fileOut("../bin/output.ppm", ios::out);
 	int nx = 200;
 	int ny = 100;
+	fstream fileOut("../bin/output.ppm", ios::out);
 	fileOut << "P3\n" << nx << " " << ny << "\n255\n";
+	vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+	vec3 horizontal(4.0f, 0, 0);
+	vec3 vertical(0, 2.0f, 0);
+	vec3 origin(0, 0, 0);
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2f;
-			int ir = int(255.99f * r);
-			int ig = int(255.99f * g);
-			int ib = int(255.99f * b);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 col = color(r);
+			int ir = int(255.99f * col.r());
+			int ig = int(255.99f * col.g());
+			int ib = int(255.99f * col.b());
+
 			fileOut << ir << " " << ig << " " << ib << "\n";
 		}
 	}
